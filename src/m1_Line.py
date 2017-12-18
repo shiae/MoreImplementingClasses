bdae29fffbcc622990518eeb8b6f58875e6baccd
Allison Shi <shiae@rose-hulman.edu>
2017-12-18 16:12:46 -0500

    m1 DONE: 4

--- src/m1_Line.py
+++ src/m1_Line.py
@@ -229,7 +229,6 @@ class Line(object):
         self.start = start.clone()
         self.end = end.clone()
 
-
     def __repr__(self):
         """
         What comes in:
@@ -330,7 +329,7 @@ class Line(object):
           :rtype: Line
         """
         # --------------------------------------------------------------
-        # TODO: 4.
+        # DONE: 4.
         #   a. READ the above specification, including the Example.
         #        ** ASK QUESTIONS AS NEEDED. **
         #        ** Be sure you understand it, ESPECIALLY the Example.
@@ -338,6 +337,8 @@ class Line(object):
         #        The tests are already written (below).
         #        They include the Example in the above doc-string.
         # --------------------------------------------------------------
+
+        return Line(self.start, self.end)
 
     def reverse(self):
         """
